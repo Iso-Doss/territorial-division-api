fb98401f079fb305b598c97d4772afcdb7327526
Iso-Doss <dosou.israel48@gmail.com>
2024-01-05 21:06:59 +0100

    Mise ajour des migrations

--- database/migrations/2023_10_20_000012_create_country_international_organisation_table.php
+++ database/migrations/2023_10_20_000012_create_country_international_organisation_table.php
@@ -1,13 +1,11 @@
 <?php
 
 use App\Models\Country;
-use App\Models\InternationalOrganisation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
